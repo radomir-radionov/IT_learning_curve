aca69fa018e562d04839fea95aa9b9eb8e36fe72
radomir <radomir.radionov26@gmail.com>
2026-04-11 23:55:28 +0300

    [dev] delete unused code

--- next-supabase/src/services/supabase/lib/getCurrentUser.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from "react"
-import { createClient } from "../server"
-
-export const getCurrentUser = cache(async () => {
-  const supabase = await createClient()
-  return (await supabase.auth.getUser()).data.user
-})
--- next-supabase/src/services/supabase/schemas/rooms.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod"
-
-export const createRoomSchema = z.object({
-  name: z.string().min(1).trim(),
-  isPublic: z.boolean(),
-})
